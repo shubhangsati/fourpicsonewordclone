579c61442101cc17303d76430d4a0baeb598dc1f
Shubhang Sati <shubhang16198@iiitd.ac.in>
2018-02-28 02:38:11 +0530

    basic layout done

--- js/imagefullsize.js
+++ js/imagefullsize.js
@@ -1,11 +1,11 @@
 function viewfullimage(element) {
 	var src = element.src;
 	$("#fullimage").attr("src", src);
-	$(".hintimage").css("display", "none");
+	$("#smallimages").css("display", "none");
 	$("#fullimagediv").css("display", "block");
 }
 
 function goback() {
 	$("#fullimagediv").css("display", "none");
-	$(".hintimage").css("display", "inline");
+	$("#smallimages").css("display", "inline");
 }
