3a8c47de385de1089df632ff2b4298e8b79e5186
Shubhang Sati <shubhang16198@iiitd.ac.in>
2018-09-24 03:23:01 +0530

    added comments to game.js

--- js/game.js
+++ js/game.js
@@ -1,4 +1,6 @@
-//CONSTANTS
+// object that maps the level number to its answer
+// you can add more levels just by adding another 
+// entry to this object and incrementing nooflevels by one
 var leveltoanswers = {
 	1 : "ELON",
 	2 : "SUMMER",
@@ -6,49 +8,57 @@ var leveltoanswers = {
 	4 : "BAND"
 }
 
-var currentlevel = 1;
-var tempanswer = leveltoanswers[currentlevel];
-var noofhints = 2;
-//
+var nooflevels = 4; // number of levels
+var currentlevel = 1; // starting level
 
-//
+// tempanswer will store the string of characters that 
+// are supposed to be in the answer string
+// but have not yet been added by the player
+var tempanswer = leveltoanswers[currentlevel];
+
+var noofhints = 2; // number of hints
+
+// mapping between ith option and a boolean which is true
+// if the ith option has not been used yet.
 var options = {
 	0 : true, 1 : true, 2 : true, 3 : true, 4 : true, 5 : true, 6 : true, 7 : true, 8 : true, 9 : true, 10 : true, 11 : true, 12 : true, 13 : true, 14 : true, 15 : true, 16 : true, 17 : true
 }
 
-var blanks = {}
+var blanks = {} // object for blanks
 
-var letters = {}
+var letters = {} // object for letters
 
-var freq = {}
+var freq = {} // stores the frequency of occurrence of a letter in the answer string
 
-var tempfreq = {}
-//
+var tempfreq = {} // stores the frequency of occurrence of a letter in the current submission
 
 
 function main() {
+	// the main function
 	var alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	var answer = leveltoanswers[currentlevel];
 	for (var i = 0; i < 26; ++i) {
+		// set all frequency counts to 0
 		freq[alphabets[i]] = 0;
 		tempfreq[alphabets[i]] = 0;
 	}
 	for (var i = 0; i < answer.length; ++i)
-		freq[answer[i]] += 1;
+		freq[answer[i]] += 1; // count the frequency of characters in the answer string
 
-	addimages(currentlevel);
+	addimages(currentlevel); // add images for the current level
+	// set onclick attribute of the images to viewfullimage
+	// viewfullimage(defined in imagefullsize.js) expands the clicked image
 	$(".hintimage").attr("onclick", "viewfullimage(this)");
-	addblanks(currentlevel);
-	addoptions(currentlevel);
+	addblanks(currentlevel); // add blanks for this level
+	addoptions(currentlevel); // add letter options for this level
 }
 
-//CHECK IF CORRECT
-//START
-
+// check if the current submission is correct
+// START
 function getsubmission() {
 	var x = "";
 	$(".blank").each(function(item, element) {
-		x += element.innerHTML;
+		x += element.innerHTML; // add individual characters from blanks
 	});
 
 	return x;
@@ -56,19 +66,20 @@ function getsubmission() {
 
 function checkifcorrect(level) {
 	var submission = getsubmission();
-	if (submission == leveltoanswers[level])
+	if (submission === leveltoanswers[level])
 		return true;
 	return false;
 }
-
 //END
 
 
 //To add images
 //START
 function addimages(level) {
-	$("#smallimages").empty();
-	var dir = "./img/level" + level + "/";
+	$("#smallimages").empty(); // clear the #smallimages div
+	var dir = "./img/level" + level + "/"; // directory base url
+	
+	// add image elements
 	for (var i = 1; i <= 2; ++i) {
 		var imagelocation = dir + i + ".jpg" ;
 		$("#smallimages").append("<img src=" + imagelocation + " class='hintimage'>");
@@ -86,12 +97,14 @@ function addimages(level) {
 //To add blanks
 //START
 function addblanks(level) {
-	$("#blanks").empty();
+	$("#blanks").empty(); // clear the #blanks div
 	var answer = leveltoanswers[level];
 	for (var i = 0; i < answer.length; ++i) {
 		$("#blanks").append("<span class='blank' onclick='deselect(\"" + i + "\")'>_</span>");
 		blanks[i] = null;
 	}
+
+	// add a hint button
 	$("#blanks").append("<div id='hintbutton' onclick='hint(" + level + ")'><i class='fas fa-lightbulb' style='font-size: 60px;'></i><br><span id='noofhints'></span></div><br><br>")
 	$("#noofhints")[0].innerHTML = noofhints + " hint(s) remaining";
 }
@@ -102,11 +115,13 @@ function addblanks(level) {
 //To add options
 //START
 function addoptions(level) {
+	 // create a string containing the answer mixed with random alphabets
 	var s = createstring(level);
 	for (var i = 0; i < 18; ++i) {
 		letters[i] = s[i];
 	}
-	$("#letters").empty();
+	$("#letters").empty(); // empty the #letters div
+	// add elements dynamically to the #letters div
 	for (var i = 0; i < 9; ++i)
 		$("#letters").append("<span class='letter' onclick='addletter(\"" + s[i] + "\", " + i + ")'>" + s[i] + "</span>");
 	$("#letters").append("<br>");
@@ -114,6 +129,7 @@ function addoptions(level) {
 		$("#letters").append("<span class='letter' onclick='addletter(\"" + s[i] + "\", " + i + ")'>" + s[i] + "</span>");
 }
 
+// function for random shuffling
 String.prototype.shuffle = function() {
 
 	var that = this.split("");
@@ -125,6 +141,11 @@ String.prototype.shuffle = function() {
 	return that.join("");
 }
 
+/* 
+creates string by padding the answer string with
+random letter that are not already in the answer 
+string. Then returns the shuffled string in the end.
+*/
 function createstring(level) {
 	var answer = leveltoanswers[level];
 	var numberremaining = 18 - answer.length;
@@ -135,21 +156,24 @@ function createstring(level) {
 		if (answer.indexOf(alphabets[i]) == -1)
 			possible += alphabets[i];
 	}
+	possible = possible.shuffle();
 	for (var i = 0; i < numberremaining; ++i)
-		s += possible.charAt(Math.floor(Math.random() * s.length));
+		s += possible[i];
 	s = s.shuffle();
 	return s;
 }
-
 //END
 
-
+// updates the tempanswer variable which stores
+// the characters that are in the current level's answer
+// but not in the current submission by the user.
+// tempanswer will be used to add hints
 function updatetempanswer() {
 	var answer = leveltoanswers[currentlevel];
 	var s = "";
 	$(".blank").each(function(item, element){
 		var xxx = element.innerHTML;
-		if (xxx == "_" || xxx != answer[item]) {
+		if (xxx === "_" || xxx != answer[item]) {
 			s += answer[item];
 		}
 	});
@@ -158,7 +182,7 @@ function updatetempanswer() {
 
 //To add letters to the submission
 //START
-
+// check if all blanks are currently filled
 function allfilled() {
 	var isempty = false;
 	$(".blank").each(function(item, element) {
@@ -169,6 +193,7 @@ function allfilled() {
 	return !isempty;
 }
 
+// find the first empty blank
 function findfirstvacant() {	
 	var elementtoreturn;
 	var index;
@@ -180,28 +205,42 @@ function findfirstvacant() {
 			break;
 		}
 	}
-
+	// return the first empty html element(of class blank) and the index
+	// of that element in the array containing elements of class blank
 	return [elementtoreturn, index];
 }
 
 function addletter(lettertoadd, index) {
+	// function to add letter to the current submission.
+	// this is called when one of the options is clicked.
 	if (options[index] == false) {
+		// if letter is already used, do nothing and return
 		return;
 	}
 	else {
+		// find first empty blank
 		var ffv = findfirstvacant();
 		var element = ffv[0];
 		var elindex = ffv[1];
+
 		element.innerHTML = lettertoadd;
-		options[index] = false;
+		options[index] = false; // mark the clicked option as false
+
+		// set onclick to null and change styles
 		$(".letter")[index].onclick = null;
 		$(".letter")[index].style.cursor = "not-allowed";
 		$(".letter")[index].style.background = "green";
+
+		// store a reference to the letter option which was clicked to add
+		// to this(first empty) blank. This will be used to deselect this option
 		blanks[elindex] = index;
-		updatetempanswer();
+		updatetempanswer(); // update the tempanswer variable
 		tempfreq[lettertoadd] += 1;
+
+		// if all blanks have been filled check whether the submission
+		// is correct or not. Call nextmove().
 		if (allfilled())
-			nextmove();
+			nextmove(); 
 	}
 }
 
@@ -212,39 +251,47 @@ function addletter(lettertoadd, index) {
 //START
 
 function deselect(elindex) {
+	// function to deselect a letter option by clicking on the 
+	// corresponding blank
 	if ($(".blank")[elindex].innerHTML == "_") {
+		// if the clicked blank is already blank ("_")
 		return;
 	}
+
 	var lettertoremove = $(".blank")[elindex].innerHTML;
-	tempfreq[lettertoremove] -= 1;
-	$(".blank")[elindex].innerHTML = "_";
-	var index = blanks[elindex];
-	options[index] = true;
-	$(".letter")[index].onclick = function() {
+	tempfreq[lettertoremove] -= 1; // reduce the frequency count by one
+	$(".blank")[elindex].innerHTML = "_"; // set the current .blank element's innerHTML
+	var index = blanks[elindex]; // get index of the option to deselect
+	options[index] = true; // enable the option
+	$(".letter")[index].onclick = function() { // set onclick attribute
 		addletter(letters[index], index);
 	};
+	// set the styles of the deselected option
 	$(".letter")[index].style.cursor = "pointer";
 	$(".letter")[index].style.background = "#222";
 	$(".blank").css("color", "black");
-	updatetempanswer();
+	updatetempanswer(); // update tempanswer variable
 }
 
 //END
-
-
-
-
-
 
 //NEXT MOVES
 //START
 function nextmove() {
 	if (checkifcorrect(currentlevel)) {
-		if (currentlevel == 4) {
+		// if current submission is correct
+		
+		if (currentlevel === nooflevels) {
+			// if the answer for the last level is correct
+			// redirect to congratulations page
 			window.location = "congrats.html";
 		}
-		currentlevel += 1;
+
+		// show next level
+		currentlevel++;
 		tempanswer = leveltoanswers[currentlevel];
+
+		// reset everything
 		options = {
 			0 : true, 1 : true, 2 : true, 3 : true, 4 : true, 5 : true, 6 : true, 7 : true, 8 : true, 9 : true, 10 : true, 11 : true, 12 : true, 13 : true, 14 : true, 15 : true, 16 : true, 17 : true
 		}
@@ -256,26 +303,30 @@ function nextmove() {
 		}
 
 		noofhints = 2;
-
-		main();
+		main(); // call main to update images, blanks and letter options
 	}
 
 	else {
+		// current submission is incorrect, mark incorrect.
 		markincorrect();
 	}
 }
 
 
 function markincorrect() {
+	// set color to red to show incorrect submission
 	$(".blank").css("color", "red");
 }
 //END
 
 
 //HINT
+// Adding a hint will add a character in the correct position as it 
+// appears in the final answer string
 //START
-
-function findlast(letter) {
+function findLetter(letter) {
+	// function to return the index of the letter option element 
+	// which will be marked as selected when the hint letter is added
 	var index;
 	$(".letter").each(function(item, element) {
 		if (element.innerHTML == letter) {
@@ -286,23 +337,37 @@ function findlast(letter) {
 }
 
 function getRandomLetter() {
-	var position = Math.floor(Math.random() * tempanswer.length);
-	var letter = tempanswer.charAt(position);
+	// function to get one letter from the tempanswer string
+	// this letter is chosen at random
+	var position = Math.floor(Math.random() * tempanswer.length); // random index
+	var letter = tempanswer.charAt(position); // get character at index position
+
+	// remove this letter from the tempanswer string
 	tempanswer = tempanswer.substr(0, position) + tempanswer.substr(position + 1, tempanswer.length);
-	var pos;
-	var answer = leveltoanswers[currentlevel];
+	var pos; // pos will store the index where the hint letter should be added
+	var answer = leveltoanswers[currentlevel]; // get current level's answer
 	for (var i = 0; i < answer.length; ++i) {
 		if (answer[i] == letter && $(".blank")[i].innerHTML != letter) {
 			pos = i;
 			break;
 		}
 	}
-	return [letter, pos];
+
+	return [letter, pos]; // return the hint letter and its position in the final answer string
 }
 
 function addhint(lettertoadd, index, position) {
+	// function to add hints given the hint letter, its position
+	// and the index of the letter option to be marked as selected
 	var answer = leveltoanswers[currentlevel];
-	if (tempfreq[lettertoadd] == freq[lettertoadd]) {
+	if (tempfreq[lettertoadd] == freq[lettertoadd]) { 
+		/*
+		if the hint letter appears the same number of times in both
+		the current submission and the answer string, we need to remove
+		one of these letter from the current submission (since we already
+		know that one of the hint letter is not in its correct position
+		in the current submission).
+		*/
 		var firstfoundat;
 		for (var i = 0; i < answer.length; ++i) {
 			if ($(".blank")[i].innerHTML == lettertoadd) {
@@ -315,17 +380,19 @@ function addhint(lettertoadd, index, position) {
 		tempfreq[lettertoadd] -= 1;
 	}
 
+	// get the blank at index = position
 	var element = $(".blank")[position];
 	var elindex = position;
 
-	if (element.innerHTML != "_") {
-		var lettertoremove = element.innerHTML;
+	if (element.innerHTML != "_")
 		deselect(position);
-	}
 
+	// add the hint letter in the blank
 	element.innerHTML = lettertoadd;
-	options[index] = false;
-	$(".letter")[index].onclick = null;
+	options[index] = false; // disable the letter option at the given index 
+	$(".letter")[index].onclick = null; // disable the click event handler
+
+	// the same code as addletter() method above
 	$(".letter")[index].style.cursor = "not-allowed";
 	$(".letter")[index].style.background = "green";
 	blanks[elindex] = index;
@@ -334,25 +401,21 @@ function addhint(lettertoadd, index, position) {
 	tempfreq[lettertoadd] += 1;
 	if (allfilled())
 		nextmove();
-	else {
-		return elindex;
-	}
 }
 
 function hint() {
+	// function to add hints
 	if (noofhints <= 0) {
+		// if all hints have been used, do nothing and return
 		return;
 	}
-	var grl = getRandomLetter();
-	console.log(grl);
+	var grl = getRandomLetter(); // get the hint letter from tempanswer string
 	var letter = grl[0];
 	var position = grl[1];
-	var index = findlast(letter);
-	//console.log(index);
-	var elindex = addhint(letter, index, position);
-	noofhints -= 1;
-
-	$("#noofhints")[0].innerHTML = noofhints + " hint(s) remaining";
+	var index = findLetter(letter); // find the hint letter in the letter options
+	noofhints--; // decrement number of hints by one
+	addhint(letter, index, position); // add hint
+	$("#noofhints")[0].innerHTML = noofhints + " hint(s) remaining"; // update the innerHTML for number of hints remaining
 }
 
 //END
